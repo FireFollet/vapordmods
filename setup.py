be474dfb79f186b1850b01f570eecce209e59eb6
FireFollet <103963118+FireFollet@users.noreply.github.com>
2022-04-19 22:47:26 -0400

    Initial

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='vapordmods',
+    version='0.5.0',
+    packages=[''],
+    url='',
+    license='',
+    author='FireFollet',
+    author_email='',
+    description=''
+)
--- src/vapordmods/vapordmods.py
+++ src/vapordmods/vapordmods.py
@@ -6,26 +6,20 @@ import json
 
 
 class vapordmods:
-    __API_URL_LATEST = "https://thunderstore.io/api/experimental/package/{}/{}/"
-    __API_URL_VERSION = "https://thunderstore.io/api/experimental/package/{}/{}/{}"
-    __CFG_FILENAME = 'vapord_mods.yml'
-    __MANIFESTS_DIR = 'vapord_manifests'
-    __MANIFESTS_TMODS_DIR = 'tmods'
-    __MANIFESTS_NMODS_DIR = 'nmods'
-    __MANIFESTS_SWORKSHOP_DIR = 'sworkshop'
+    __THUNDERSTORE_API_URL_LATEST = "https://thunderstore.io/api/experimental/package/{}/{}/"
+    __THUNDERSTORE_API_URL_VERSION = "https://thunderstore.io/api/experimental/package/{}/{}/{}"
+    __CFG_FILENAME = 'vapordmods.yml'
+    __MANIFESTS_DIR = 'vapordmods_manifests'
+    __THUNDERSTORE_NAME = 'thunderstore'
+    __STEAMWORKSHOP_NAME = 'steamworkshop'
 
     def __init__(self, install_dir: str):
         self.cfg_filename = os.path.join(install_dir, self.__CFG_FILENAME)
         self.cfg_data = None
-        self.tmods_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__MANIFESTS_TMODS_DIR)
-        self.tmods_mods = []
-        self.tmods_manifests = []
-        self.nmods_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__MANIFESTS_NMODS_DIR)
-        self.nmods_mods = []
-        self.nmods_manifests = []
-        self.sworkshop_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__MANIFESTS_SWORKSHOP_DIR)
-        self.sworkshop_mods = []
-        self.sworkshop_manifests = []
+        self.thunderstore_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__THUNDERSTORE_NAME)
+        self.thunderstore_mods = []
+        self.steamworkshop_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__STEAMWORKSHOP_NAME)
+        self.steamworkshop_mods = []
 
         if os.path.exists(self.cfg_filename):
             if os.path.isfile(self.cfg_filename) and os.access(self.cfg_filename, os.R_OK):
@@ -34,12 +28,12 @@ class vapordmods:
                 raise PermissionError("'{}' is not a file or cannot be read.".format(self.cfg_filename))
         elif os.path.exists(install_dir):
             cfg_file = open(self.cfg_filename, 'w')
-            cfg_file.write('mods:\n  tmods:\n\n  nmods:\n\n  sworkshop:\n')
+            cfg_file.write('mods:\n  ' + self.__THUNDERSTORE_NAME + ':\n\n  ' + self.__STEAMWORKSHOP_NAME + ':\n')
             cfg_file.close()
         else:
             raise FileNotFoundError("Cannot create or read '{}'.".format(self.cfg_filename))
 
-        list_mods = {self.tmods_dir, self.nmods_dir, self.sworkshop_dir}
+        list_mods = {self.thunderstore_dir, self.steamworkshop_dir}
 
         for mod in list_mods:
             if not os.path.exists(mod):
@@ -50,53 +44,63 @@ class vapordmods:
         self.cfg_data = yaml.safe_load(f)
         f.close()
 
-    def tmodapi(self, namespace, name, version=None):
+    def thunderstoreapi(self, namespace, name, version=None):
         if not version:
-            r = requests.get(self.__API_URL_LATEST.format(namespace, name))
+            r = requests.get(self.__THUNDERSTORE_API_URL_LATEST.format(namespace, name))
         else:
-            r = requests.get(self.__API_URL_VERSION.format(namespace, name, version))
+            r = requests.get(self.__THUNDERSTORE_API_URL_VERSION.format(namespace, name, version))
         if r.status_code == 200:
             return r.json()
         else:
             return None
 
-    def getmodinfo(self):
-        if self.cfg_data['mods']:
-            modstatus = {'tmods': {}, 'nmods': {}, 'sworkshop': {}}
-            # tmods
-            if self.cfg_data['mods']['tmods']:
-                for namespace in self.cfg_data['mods']['tmods']:
-                    for name, value in namespace:
-                        currentmanifest = None
-                        filename = os.path.join(self.tmods_dir, namespace + '_' + name + '.json')
-                        if os.path.exists(filename):
-                            f = open(filename, 'r')
-                            currentmanifest = json.load(f)
-                            f.close()
+    def buildupdateinfofromthunderstore(self):
+        if self.cfg_data['mods'][self.__THUNDERSTORE_NAME]:
+            depotdata = {}
+            for namespace in self.cfg_data['mods'][self.__THUNDERSTORE_NAME]:
+                for name, value in namespace:
+                    currentmanifest = None
+                    filename = os.path.join(self.thunderstore_dir, namespace + '_' + name + '.json')
+                    if os.path.exists(filename):
+                        f = open(filename, 'r')
+                        currentmanifest = json.load(f)
+                        f.close()
 
-                        if value and currentmanifest:
-                            if value == currentmanifest['latest']['version_number']:
-                                data = {'currentversion': value, 'latestversion': 'current_match_requirement', 'needupdate': False, 'newmanifest': None}
-                                if namespace not in modstatus['tmods']:
-                                    modstatus['tmods'][namespace] = {name: data}
-                                else:
-                                    modstatus['tmods'][namespace][name] = data
-                        else:
-                            r = self.tmodapi(namespace, name, value)
-                            if r:
-                                latestversion = r['latest']['version_number']
+                    if value and currentmanifest:
+                        if value == currentmanifest['latest']['version_number']:
+                            data = {'currentversion': value, 'latestversion': 'current_match_requirement',
+                                    'needupdate': False, 'newmanifest': None}
+                            if namespace not in depotdata[self.__THUNDERSTORE_NAME]:
+                                depotdata[self.__THUNDERSTORE_NAME][namespace] = {name: data}
+                            else:
+                                depotdata[self.__THUNDERSTORE_NAME][namespace][name] = data
+                    else:
+                        r = self.thunderstoreapi(namespace, name, value)
+                        if r:
+                            with r.json() as manifest:
+                                latestversion = manifest['latest']['version_number']
                                 if currentmanifest:
                                     currentversion = currentmanifest['latest']['version_number']
-                                    if currentmanifest['latest']['version_number'] == r['latest']['version_number']:
+                                    if currentmanifest['latest']['version_number'] == manifest['latest']['version_number']:
                                         needupdate = False
                                         newmanifest = None
                                     else:
                                         needupdate = True
-                                        newmanifest = r
+                                        newmanifest = manifest
                                 else:
                                     currentversion = 'not_installed'
                                     needupdate = True
-                                    newmanifest = r
-                            else:
-                                logging.error("Impossible de trouver l'info pour ...")
+                                    newmanifest = manifest
+                        else:
+                            logging.error("Impossible de trouver l'info pour ...")
+
+    def buildupdateinfofromsteam(self):
+        pass
+
+    def getmodinfo(self):
+        if self.cfg_data['mods']:
+            modstatus = {self.__THUNDERSTORE_NAME: {}, self.__STEAMWORKSHOP_NAME: {}}
+
             return modstatus
+        else:
+            raise ValueError("Erreur a faire")
