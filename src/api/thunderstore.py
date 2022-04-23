ca0cfd7fe1d1cd989e6a968be97d42bd139aec0b
FireFollet <103963118+FireFollet@users.noreply.github.com>
2022-04-22 23:27:35 -0400

    Initial

--- src/api/thunderstore.py
+++ src/api/thunderstore.py
@@ -5,10 +5,17 @@ __THUNDERSTORE_API_URL_VERSION = "https://thunderstore.io/api/experimental/packa
 
 
 async def get_update(namespace: str, name: str, version: str = None, api_key: str = None) -> dict:
+    req_community = None
     if not version:
         request = __THUNDERSTORE_API_URL_LATEST.format(namespace, name)
     else:
         request = __THUNDERSTORE_API_URL_VERSION.format(namespace, name, version)
+        req_community = __THUNDERSTORE_API_URL_LATEST.format(namespace, name)
+        async with aiohttp.request('GET', req_community) as resp_community:
+            if resp_community.status == 200:
+                c = await resp_community.json()
+            else:
+                return {'status': resp_community.status, 'data': resp_community.reason}
 
     async with aiohttp.request('GET', request) as resp:
         if resp.status == 200:
@@ -17,13 +24,15 @@ async def get_update(namespace: str, name: str, version: str = None, api_key: st
                 version = j['latest']['version_number']
                 dependencies = j['latest']['dependencies']
                 download_url = j['latest']['download_url']
+                community = j['community_listings'][0]['community']
             else:
                 version = j['version_number']
                 dependencies = j['dependencies']
                 download_url = j['download_url']
+                community = c['community_listings'][0]['community']
 
             data = {'app': namespace, 'mods': name, 'version': version, 'dependencies': dependencies,
-                    'download_url': str(download_url).replace('https://', 'https://' + j['community_listings'][0]['community'] + '.'), 'manifest': j}
+                    'download_url': str(download_url).replace('https://', 'https://' + community + '.'), 'manifest': j}
             return {'status': resp.status, 'data': data}
         else:
             return {'status': resp.status, 'data': resp.reason}
--- src/vapordmods/vapordmods.py
+++ src/vapordmods/vapordmods.py
@@ -2,7 +2,7 @@ import os
 import yaml
 import logging
 import json
-from src.api import thunderstore
+from src.api import thunderstore, nexusmods
 
 
 class vapordmods:
@@ -10,17 +10,13 @@ class vapordmods:
     __MANIFESTS_DIR = 'vapordmods_manifests'
     __THUNDERSTORE_NAME = 'thunderstore'
     __NEXUSMODS_NAME = 'nexusmods'
-    __STEAMWORKSHOP_NAME = 'steamworkshop'
 
     def __init__(self, install_dir: str):
         self.cfg_filename = os.path.join(install_dir, self.__CFG_FILENAME)
         self.cfg_data = None
+        self.mods_info = None
         self.thunderstore_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__THUNDERSTORE_NAME)
-        self.thunderstore_mods = []
         self.nexusmods_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__NEXUSMODS_NAME)
-        self.thunderstore_mods = []
-        self.steamworkshop_dir = os.path.join(install_dir, self.__MANIFESTS_DIR, self.__STEAMWORKSHOP_NAME)
-        self.steamworkshop_mods = []
 
         if os.path.exists(self.cfg_filename):
             if os.path.isfile(self.cfg_filename) and os.access(self.cfg_filename, os.R_OK):
@@ -29,63 +25,95 @@ class vapordmods:
                 raise PermissionError("'{}' is not a file or cannot be read.".format(self.cfg_filename))
         elif os.path.exists(install_dir):
             cfg_file = open(self.cfg_filename, 'w')
-            cfg_file.write('mods:\n  ' + self.__THUNDERSTORE_NAME + ':\n\n  ' + self.__NEXUSMODS_NAME +
-                           ':\n\n  ' + self.__STEAMWORKSHOP_NAME + ':\n')
+            cfg_file.write('mods:\n  ' + self.__THUNDERSTORE_NAME + ':\n\n  ' + self.__NEXUSMODS_NAME + ':\n')
             cfg_file.close()
         else:
             raise FileNotFoundError("Cannot create or read '{}'.".format(self.cfg_filename))
 
-        list_mods = {self.thunderstore_dir, self.nexusmods_dir, self.steamworkshop_dir}
+        list_mods = {self.thunderstore_dir, self.nexusmods_dir}
 
         for mod in list_mods:
             if not os.path.exists(mod):
                 os.makedirs(mod)
 
-    async def reload_cfg(self):
+    def reload_cfg(self):
         f = open(self.cfg_filename, "r")
         self.cfg_data = yaml.safe_load(f)
         f.close()
 
-    async def get_mods_info(self, api_key: str = None):
+    async def refresh_mods_info(self, api_key: str = None):
         if self.cfg_data['mods']:
-            modstatus = {self.__THUNDERSTORE_NAME: {}, self.__NEXUSMODS_NAME: {}, self.__STEAMWORKSHOP_NAME: {}}
+            modstatus = {self.__THUNDERSTORE_NAME: {}, self.__NEXUSMODS_NAME: {}}
             for provider in self.cfg_data['mods']:
+                if provider not in (self.__THUNDERSTORE_NAME, self.__NEXUSMODS_NAME):
+                    break
+                if not self.cfg_data['mods'][provider]:
+                    break
+
                 for app in self.cfg_data['mods'][provider]:
-                    for mods, value in app:
+                    if app is None:
+                        break
+                    for mod in self.cfg_data['mods'][provider][app]:
+                        if mod is None:
+                            break
+
+                        if type(mod) == str:
+                            modname = mod
+                            modversion = None
+                        else:
+                            list_mod = list(mod.items())[0]
+                            modname = list_mod[0]
+                            modversion = list_mod[1]
+
                         currentmanifest = None
-                        filename = os.path.join(self.thunderstore_dir, app + '_' + mods + '.json')
+                        filename = os.path.join(self.thunderstore_dir, app + '_' + modname + '.json')
                         if os.path.exists(filename):
                             f = open(filename, 'r')
                             currentmanifest = json.load(f)
                             f.close()
 
-                        if value and currentmanifest:
-                            if value == currentmanifest['latest']['version_number']:
-                                data = {'currentversion': value, 'latestversion': 'current_match_requirement',
-                                        'needupdate': False, 'newmanifest': None}
-                                if app not in modstatus[self.__THUNDERSTORE_NAME]:
-                                    modstatus[self.__THUNDERSTORE_NAME][app] = {mods: data}
-                                else:
-                                    modstatus[self.__THUNDERSTORE_NAME][app][mods] = data
+                        data = None
+                        if modversion and currentmanifest:
+                            if currentmanifest['latest']:
+                                installed_version = currentmanifest['latest']['version_number']
+                            else:
+                                installed_version = currentmanifest['version_number']
+                            if modversion == installed_version:
+                                data = {'currentversion': modversion, 'latestversion': 'current_match_requirement',
+                                        'needupdate': False, 'download_link': 'not_required', 'newmanifest': None}
                         else:
-                            r = await locals()[provider].get_update(app, mods, value, api_key)
+                            r = await globals()[provider].get_update(app, modname, modversion, api_key)
                             if r:
-                                with r.json() as manifest:
-                                    latestversion = manifest['latest']['version_number']
-                                    if currentmanifest:
-                                        currentversion = currentmanifest['latest']['version_number']
-                                        if currentmanifest['latest']['version_number'] == manifest['latest']['version_number']:
-                                            needupdate = False
-                                            newmanifest = None
-                                        else:
-                                            needupdate = True
-                                            newmanifest = manifest
+                                latestversion = r['data']['version']
+                                download_link = None
+                                if currentmanifest:
+                                    currentversion = currentmanifest['version']
+                                    if currentmanifest['version'] == r['data']['version']:
+                                        needupdate = False
+                                        download_link = 'not_required'
+                                        newmanifest = None
                                     else:
-                                        currentversion = 'not_installed'
                                         needupdate = True
-                                        newmanifest = manifest
+                                        ddownload_link = r['data']['download_url']
+                                        newmanifest = r['data']['manifest']
+                                else:
+                                    currentversion = 'not_installed'
+                                    needupdate = True
+                                    download_link = r['data']['download_url']
+                                    newmanifest = r['data']['manifest']
+
+                                data = {'currentversion': currentversion, 'latestversion': latestversion,
+                                        'needupdate': needupdate, 'download_link': download_link, 'newmanifest': newmanifest}
                             else:
                                 logging.error("Impossible de trouver l'info pour ...")
-                return modstatus
+
+                        if app not in modstatus[provider]:
+                            modstatus[provider][app] = {modname: data}
+                        else:
+                            modstatus[provider][app][modname] = data
+                self.mods_info = modstatus
         else:
             raise ValueError("Erreur a faire")
+
+    async def update_mods(self):
+        pass
