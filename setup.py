4c602f6567b6d7262079b83f49a3a1b38b7d3954
FireFollet <firefollet@outlook.com>
2022-05-18 23:35:40 -0400

    Initial

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='vapordmods',
-    version='0.1.3',
+    version='0.1.4',
     author='FireFollet',
     author_email='',
     description='Manage multiples mods provider like Thunderstore, Nexismods and Steam Workshop.',
