ef08641676caad903287b2a29d5860d51d651ea8
FireFollet <firefollet@outlook.com>
2022-05-20 16:07:46 -0400

    Initial

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='vapordmods',
-    version='0.1.8',
+    version='0.1.9',
     author='FireFollet',
     author_email='',
     description='Manage multiples mods provider like Thunderstore, Nexismods and Steam Workshop.',
@@ -21,7 +21,7 @@ setuptools.setup(
                       'aiofiles~=0.8.0',
                       'pandas~=1.4.2',
                       'cerberus~=1.3.4'],
-    packages=setuptools.find_packages(where='vapordmods'),
+    packages=setuptools.find_packages(),
     python_requires=">=3.8",
     license='MIT',
     classifiers=[
