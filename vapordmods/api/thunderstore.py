ea113fa78863ce172f19ad37e19288f48c163166
FireFollet <firefollet@outlook.com>
2022-05-20 16:13:47 -0400

    Initial

--- vapordmods/api/thunderstore.py
+++ vapordmods/api/thunderstore.py
@@ -1,9 +1,8 @@
 import aiohttp
 import logging
-
-from ..api.base import BaseApi
-
 from vapordmods.api.base import BaseApi
+
+api_logger = logging.getLogger(__name__)
 
 
 class thunderstore(BaseApi):
